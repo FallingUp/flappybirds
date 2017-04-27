a984cad8eab1e2035a977f1c3a8dbb24771dba95
Charlie Brown <b1308189@student.southstaffs.ac.uk>
2017-04-27 11:18:41 +0100

    main updated

--- main.js
+++ main.js
@@ -3,17 +3,56 @@ var mainStage = {
     preload: function() {
         //This function will be executed at the beginning
         //Thats where we load the images and sound
+        
+        //Load the bird sprite
+        game.load.image('bird', 'assets/bird.png');
     },
     
     create: function() {
         //This function is called after the preload function
         //Here we set up the game, display sprites, etc.
+        
+        //Change the background colour of the game to Blue - for now
+        game.stage.backgroundColor = '#71c5cf';
+        
+        //Set the physics for the game
+        game.physics.startSytem(Phaser.Physics.ARCADE);
+        
+        //Display the bird at the position of x=100 and y=245
+        this.bird = game.add.sprite(100, 245, 'bird');
+        
+        //Add physics to the bird
+        //Needed for: movement, gravity, collisions, etc.
+        game.physics.arcade.enable(this.bird);
+        
+        //Add gravity to the bird to make it fall
+        this.bird.body.gravity.y = 1000;
+        
+        //Call 'jump' function when the spacebar is pressed
+        var spaceBar = game.input.keyboard.addKey(
+                        Phaser.Keyboard.SPACEBAR);
+        spaceBar.onDown.add(this.jump, this);
     },
     
     update: function() {
         //This function is called 60 times per second
         //It contains the games logic
+        
+        //Call the 'restartGame' function
+        if (this.bird.y <0 || this.bird.y >490)
+            this.restartGame();
     },
+    
+    jump: function() {
+        //Add a vertical velocity to the bird
+        this.bird.body.velocity.y = -350;
+    },
+    
+    //Restart the game
+    restartGame: function() {
+        //Start the 'main' state, which restarts the game
+        game.state.start('main');
+        },
 };
 
 //Initialise Phaser, and create a 400px by 490px game
